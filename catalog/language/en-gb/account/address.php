d546686376129ace69958bec0cf2645e03206272
Viktor Sheshliuk <viktor.sheshliuk@artilab.pro>
2026-07-12 14:34:54 +0300

    test

--- catalog/language/en-gb/account/address.php
+++ catalog/language/en-gb/account/address.php
@@ -15,7 +15,7 @@ $_['text_confirm']       = 'Are you sure?';
 
 // Entry
 $_['entry_firstname']    = 'First Name';
-$_['entry_lastname']     = 'Last Name';
+$_['entry_lastname']     = 'Last Name'; 
 $_['entry_company']      = 'Company';
 $_['entry_address_1']    = 'Address 1';
 $_['entry_address_2']    = 'Address 2';
