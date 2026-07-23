a070faaa0452c8effaf02f67d762080b7910f323
Viktor Sheshliuk <viktor.sheshliuk@artilab.pro>
2026-07-23 19:35:19 +0300

    category

--- catalog/controller/product/category.php
+++ catalog/controller/product/category.php
@@ -193,6 +193,7 @@ class ControllerProductCategory extends Controller {
 
 			$filter_data = array(
 				'filter_category_id' => $category_id,
+				'filter_sub_category' => true,
 				'filter_filter'      => $filter,
 				'sort'               => $sort,
 				'order'              => $order,
